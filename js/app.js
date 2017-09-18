e205bdbe405e29c69841319f523855ae9bc5c4df
Ginxty <james.oneill96@gmail.com>
2017-09-18 13:39:26 +0100

    randomness working kinda

--- js/app.js
+++ js/app.js
@@ -1,9 +1,23 @@
  $(function() {
  	var moleZone = $(".moleZone")
+	
+	// function selectTile(tiles) {
+ //    	var randomZone = Math.floor(Math.random()*tiles.length);
+ //    	var zone = tiles.slice(randomZone, 1)[0];
+ //    	return zone;
+ //    }
 
+ 	var randomZone = Math.floor( (Math.random() * $('ul li').length) );
+
+
+ 	console.log(randomZone)
 
  	$(".debug").click(function() {
- 		$(".moleZone").slideUp();
+ 		$(".moleZone").eq(randomZone).slideUp()
+ 		// for (var i = 0; i < moleZone.length; i++) {
+ 		// 	$(".moleZone").slideUp()
+ 		// }
+ 		// $(".moleZone").slideUp("slow");
 
  		// for (var i = 0; i < moleZone.length; i++) {
  		// 		$(moleZone[i]).slideUp(400)
