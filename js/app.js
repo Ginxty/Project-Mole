6d9762102d3c3b0496b39516dc4f9da2c1517a8e
Ginxty <james.oneill96@gmail.com>
2017-09-19 09:59:26 +0100

    point system now working kinda

--- js/app.js
+++ js/app.js
@@ -4,23 +4,18 @@
 
 
  	showMole = $(".debug").click(function() {
-	 	for (var i = 0; i < 50; i++) {
+	 	for (var i = 0; i < 999; i++) {
 	 		setTimeout(function() {
 	 			var randomZone = Math.floor( (Math.random() * $('ul li').length) );
 	 			$(".moleZone").eq(randomZone).addClass("mole");
 		 	}, 500*i)
 	 	}
 
-		$("li").click(function(){
-		    $(this).removeClass("mole");
-
-		});
-
-		$(".mole").click(function(){
+		$("ul").click(".mole", function(event){
+			$(event.target).removeClass("mole");
 		    var score = parseInt($("#score").html());
-    		score+= 100;
+    		score+= 150;
     		$("#score").html(score);
-
 		})
 
 
