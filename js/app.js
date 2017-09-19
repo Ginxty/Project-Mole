6983e0eac407970e1a75e9e4d08ead521d3aeeba
Ginxty <james.oneill96@gmail.com>
2017-09-19 15:45:50 +0100

    all mechanics working

--- js/app.js
+++ js/app.js
@@ -5,18 +5,18 @@
 
  	$(".debug").click(function() {
  		timer ();
-	 	for (var i = 0; i < 999; i++) {
+	 	for (var i = 0; i < 60; i++) {
 	 		setTimeout(function() {
 	 			var randomZone = Math.floor( (Math.random() * $('ul li').length) );
 	 			$(".moleZone").eq(randomZone).addClass("mole");
 		 	}, 500*i)
 
 	 	}
-	 	for (var i = 0; i < 999; i++) {
+	 	for (var i = 0; i < 12; i++) {
 	 		setTimeout(function() {
 	 			var randomZoneGood = Math.floor( (Math.random() * $('ul li').length) );
 	 			$(".moleZone").eq(randomZoneGood).addClass("good");
-		 	}, 1700*i)
+		 	}, 2400*i)
 
 	 	}
 
@@ -37,25 +37,30 @@
 		$("#score").html(score);
 
 	})
-	
+
  	
  	
  	$("#reset").click(function(){
-            document.location.reload(true);
-        })
+        document.location.reload(true);
+    })
 
  	function timer () {
     	var updateTimer = function() {
-    	$('#timer').each(function() {
-      		var count = parseInt($(this).html());
-      	if (count !== 0) {
-        	$(this).html(count - 1);
-      	}
-    });
-  		};
+	    	$('#timer').each(function() {
+	      		var count = parseInt($(this).html());
+		      	if (count !== 0) {
+		        	$(this).html(count - 1);
+		      	}
+		    });
+	  	};
 
-  	setInterval(updateTimer, 1000);
-};
+	  	setInterval(updateTimer, 1000);
+
+	  	setTimeout(function (){ 
+	  		$("li").removeClass("mole good"); 
+	    }, 30000);
+
+	};
 
 
  
